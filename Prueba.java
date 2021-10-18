b79a482a0f639ef3758730b2f7169b9bf46a7d79
Diego <dfreile51@gmail.com>
2021-10-18 14:22:50 +0200

    Artista Preferido

--- Prueba.java
+++ Prueba.java
@@ -2,9 +2,9 @@ public class Prueba {
 
 	public static void main(String args[]) {
 
-		System.out.println("Diego Freile Garcia");
 		System.out.println("Leon en 1999");
 		System.out.println("Leon Windows 10");
+		System.out.println("Avicii");
 
 	}
 
